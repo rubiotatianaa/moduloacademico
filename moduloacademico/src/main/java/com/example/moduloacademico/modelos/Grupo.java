a94c49bd3d1b3cf6dde564f43ed1464fdf251dcf
Natalia <caldonnatalia776@gmail.com>
2025-08-20 12:54:21 -0500

    Creacion de Entidad Grupo junto con campos basicos y relacion con materia

--- moduloacademico/src/main/java/com/example/moduloacademico/modelos/Grupo.java
+++ moduloacademico/src/main/java/com/example/moduloacademico/modelos/Grupo.java
@@ -1,4 +1,58 @@
 package com.example.moduloacademico.modelos;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Grupo {
-}
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String nombre;
+
+        private Integer semestre;
+
+        @ManyToOne
+        @JoinColumn(name = "materia_id")
+        private Materia materia;
+
+        // Constructor vacío
+        public Grupo() {
+        }
+
+        // Getters y setters
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public Integer getSemestre() {
+            return semestre;
+        }
+
+        public void setSemestre(Integer semestre) {
+            this.semestre = semestre;
+        }
+
+        public Materia getMateria() {
+            return materia;
+        }
+
+        public void setMateria(Materia materia) {
+            this.materia = materia;
+        }
+    }
+
+
